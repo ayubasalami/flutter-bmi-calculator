d5f84384d8e836584e5d513cea2dec0c5e6885f1
Ayuba Salami <Olayubasalami@gmail.com>
2022-10-07 21:22:43 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color(0xFF0AE21),
+        ),
+        scaffoldBackgroundColor: Color(0xFF0AE21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
